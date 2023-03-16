e83ae0ba6d2ab6c41695cb8a3fa1f40db308b991
jamesAtIntegratnIO <james@integratn.io>
2023-03-16 08:15:38 -0600

    initial commit for nix

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Backstage.io demo/playground";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dream2nix.url = "github:nix-community/dream2nix";
+  };
+  outputs = { self, nixpkgs, flake-utils, gitignore, dream2nix, ... }:
+    dream2nix.lib.makeFlakeOutputs {
+      systems = flake-utils.lib.defaultSystems;
+      config.projectRoot = ./.;
+      source = gitignore.lib.gitignoreSource ./.;
+    };
+}
